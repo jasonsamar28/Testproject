0ff3ddb160a18de9336a4d1feb7f1d644fbdc9a9
jasonsamar28 <107080621+jasonsamar28@users.noreply.github.com>
2022-06-15 14:18:09 -0400

    I created two classes today.

---
+++ GitHub/src/main/java/githubpractice/HubTest.java
@@ -0,0 +1,16 @@
+package githubpractice;
+
+public class HubTest {
+	
+	
+public static void main(String[] args) {
+	
+	System.out.println("This is my first Github practice");
+	
+	
+	
+}	
+	
+	
+
+}
